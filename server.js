de5d59e52e35752dd706c76bec5bdd927f424d73
thijew <thijewind@hotmail.com>
2023-04-12 19:48:27 +0200

    added ejs

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(3000)
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     console.log("does it work?")
     res.render('index')
 })
+
+
+
+
+app.listen(3000)
