4f91945761a2eb985d870f60d6a14be7b95cc287
thijew <90254525+thijew@users.noreply.github.com>
2023-04-17 00:30:54 +0200

    trying to fix db with eslint

--- config/db.js
+++ config/db.js
@@ -1,42 +1,31 @@
 require('dotenv').config();
 
-
-// Make a connecetion to mongodb
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =
- "mongodb+srv://" +
- process.env.DB-USERNAME
- ":" +
- process.env.DB-PASS +
-  "@" + 
-  process.env.DB-HOST +
-  "/" +
-  process.env.DB-NAME +
-  "?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// Make a connection to mongodb
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    // database and collection code goes here
-    // find code goes here
-    // iterate code goes here
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function runDB() {
+  const uri =
+    "mongodb+srv://" +
+    process.env.DB_USERNAME
+    ":" +
+    process.env.DB_PASS +
+    "@" +
+    process.env.DB_HOST +
+    "/" +
+    process.env.DB_NAME +
+    "?retryWrites=true&w=majority";
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+    serverApi: ServerApiVersion.v1,
+  });
+
+  client.connect(err => {
+    if (err)
+      {throw err}
+  });
 }
-run().catch(console.dir);
+  module.exports = runDB;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
 require('dotenv').config();
 
 app.set('view engine', 'ejs')
