a823775c8e299631d976b2c7c388f06115c43abb
Thije Wind <thije.wind@hva.nl>
2023-05-07 18:55:26 +0200

    form page WIP

--- routes/pages.js
+++ routes/pages.js
@@ -1,14 +1,14 @@
 // I'll be rendering all of my pages in this file
 
-const express = require('express');
-const pages = express.Router();
+// const express = require('express');
+// const pages = express.Router();
 
-pages 
-// Home page 
-.get('/', (req, res) => {
-    console.log("Welcome to the homepage")
-    res.render('pages/home')
-})
+// pages 
+// // Home page 
+// .get('/', (req, res) => {
+//     console.log("Welcome to the homepage")
+//     res.render('pages/home')
+// })
 
 
-module.exports = pages;
+// module.exports = pages;
