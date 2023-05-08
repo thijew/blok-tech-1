2d58accf3b7c8f02a97d4dce758cbc1042f0ca53
Thije Wind <thije.wind@hva.nl>
2023-05-08 16:14:51 +0200

    WIP Form

--- static/scripts/addChicken.js
+++ static/scripts/addChicken.js
@@ -1,24 +1,24 @@
-// Script to add and remove chickens
+// // Script to add and remove chickens
 
-const input = document.getElementById("chickens");
-const addButton = document.getElementById("addOne");
-const removeButton = document.getElementById("removeOne");
+// const input = document.getElementById("chickens");
+// const addButton = document.getElementById("addOne");
+// const removeButton = document.getElementById("removeOne");
 
-const addOne = () => {
-  const currentValue = parseInt(input.value);
-  input.value = currentValue + 1;
-  return false;
-};
-
-const removeOne = () => {
-  const currentValue = parseInt(input.value);
-  if (currentValue > 0) {
-    input.value = currentValue - 1;
-    console.log(currentValue);
-    return false;
-  }
+// const addOne = () => {
+//   const currentValue = parseInt(input.value);
+//   input.value = currentValue + 1;
   
-};
+// };
 
-addButton.addEventListener("click", addOne);
-removeButton.addEventListener("click", removeOne);
+// const removeOne = () => {
+//   const currentValue = parseInt(input.value);
+//   if (currentValue > 0) {
+//     input.value = currentValue - 1;
+//     console.log(currentValue);
+    
+//   }
+  
+// };
+
+// addButton.addEventListener("click", addOne);
+// removeButton.addEventListener("click", removeOne);
---
+++ static/scripts/orderTime.js
@@ -0,0 +1,2 @@
+//  document.getElementById('time-input').min = "17:00"
+//  document.getElementById('time-input').max = "22:00"
