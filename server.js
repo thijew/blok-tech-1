ef56ff9ac6de789c5d5882e85da0ddf5acaa363a
Thije Wind <thije.wind@hva.nl>
2023-05-16 23:07:13 +0200

    trying to fix admin page

--- server.js
+++ server.js
@@ -58,9 +58,15 @@ app.get('/admin', async (req, res, next) => {
     console.log("Welcome to the admin page")
 
     try {
-    const chickens = await db.collection('chickens').find().toArray()
-    res.render('pages/admin', { chickens: chickens })
+    const chickens = await db.collection('reservations').find().toArray()
     console.log(chickens)
+
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=52.56&longitude=4.61&daily=temperature_2m_max,temperature_2m_min,rain_sum&current_weather=true&timezone=Europe%2FBerlin"
+    let response = await fetch(url);
+    let weather = await response.json();
+    console.log(weather);
+
+    res.render('pages/admin', { weather: weather, chickens: chickens })
   } catch (err) {
     next(err);
   }
