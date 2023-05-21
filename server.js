e3ba41729d1e3fc4a54ec66525dbdfd2340fcc49
Thije Wind <thije.wind@hva.nl>
2023-05-21 21:16:01 +0200

    admin page WIP

--- server.js
+++ server.js
@@ -61,12 +61,12 @@ app.get('/admin', async (req, res, next) => {
     const chickens = await db.collection('reservations').find().toArray()
     // console.log(chickens)
 
-    const url = "https://api.open-meteo.com/v1/forecast?latitude=52.56&longitude=4.61&daily=temperature_2m_max,temperature_2m_min,rain_sum&current_weather=true&timezone=Europe%2FBerlin"
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=52.56&longitude=4.61&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum&timezone=Europe%2FBerlin"
     let response = await fetch(url);
     let weather = await response.json();
     // console.log(weather);
 
-    res.render('pages/admin', { weather: weather, chickens: chickens })
+    res.render('pages/admin', { weather, chickens })
   } catch (err) {
     next(err);
   }
