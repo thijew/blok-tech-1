de6fd91f2ebcbcd8a8abcc10a9eb184df4e42206
Thije Wind <thije.wind@hva.nl>
2023-05-25 12:27:44 +0200

    ADD weather icons

--- static/scripts/weather-code.js
+++ static/scripts/weather-code.js
@@ -7,15 +7,41 @@
 
         // Add an icon based on the weather code
         switch (weatherCodeIcon) {
+            case 0:
+                icon.innerHTML = '<img src="/images/wc-0.png" alt="Weather Icon">';
+                break;
+            case 1,2:
+                icon.innerHTML = '<img src="/images/wc-1.png" alt="Weather Icon">';
+                break;
             case 3:
-                icon.innerHTML = '<img src="/wc-3.png" alt="Weather Icon">';
+                icon.innerHTML = '<img src="/images/wc-3.png" alt="Weather Icon">';
                 break;
-            case 4:
-                icon.innerHTML = '<img src="path/to/weather-icon-4.png" alt="Weather Icon">';
+            case 45:
+                icon.innerHTML = '<img src="/images/wc-45.png" alt="Weather Icon">';
                 break;
-            case 5:
-                icon.innerHTML = '<img src="path/to/weather-icon-5.png" alt="Weather Icon">';
+            case 48:
+                icon.innerHTML = '<img src="/images/wc-48.png" alt="Weather Icon">';
                 break;
+            case 56:
+                icon.innerHTML = '<img src="/images/wc-56.png" alt="Weather Icon">';
+                break;
+            case 57:
+                icon.innerHTML = '<img src="/images/wc-57.png" alt="Weather Icon">';
+                break;
+            case 71:
+                icon.innerHTML = '<img src="/images/wc-71.png" alt="Weather Icon">';
+                break;
+            case 61,63,65,80,81,82:
+                icon.innerHTML = '<img src="/images/wc-82.png" alt="Weather Icon">';
+                break;
+            case 95:
+                icon.innerHTML = '<img src="/images/wc-95.png" alt="Weather Icon">';
+                break;
+            case !null:
+                icon.innerHTML = '<img src="/images/wc-unknown.png" alt="Weather Icon">';
+                break;
+                
+
             // Add cases for other weather codes and corresponding icons
             default:
                 break;
