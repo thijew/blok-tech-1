26c335c0498a1fa69f68b4b7f55c96c8e06e0a0f
thijew <thijewind@hotmail.com>
2023-06-28 22:17:57 +0200

    started again

---
+++ static/scripts/form-validation.js
@@ -0,0 +1,4 @@
+// const button = document.querySelector('.submitButton');
+// const name = document.querySelector('.submitButton');
+// const button = document.querySelector('.submitButton');
+// const button = document.querySelector('.submitButton');
