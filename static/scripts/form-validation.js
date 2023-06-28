993c893c8a1278cb2478a88727dc3c26523b0784
thijew <thijewind@hotmail.com>
2023-06-29 01:51:11 +0200

    WIP Form validation

--- static/scripts/form-validation.js
+++ static/scripts/form-validation.js
@@ -1,4 +1,26 @@
-// const button = document.querySelector('.submitButton');
-// const name = document.querySelector('.submitButton');
-// const button = document.querySelector('.submitButton');
-// const button = document.querySelector('.submitButton');
+const nameInput = document.querySelector('input#name');
+
+nameInput.addEventListener('input', (event) => {
+    const name = event.target.value;
+    const alphabeticRegex = /[A-Za-z]/;
+
+    if (!alphabeticRegex.test(name)) {
+        nameInput.setCustomValidity("Voer a.u.b alleen letters in.");
+    } else {
+        nameInput.setCustomValidity("");
+    }
+});
+
+
+const phoneNumberInput = document.querySelector('input#phone');
+
+phoneNumberInput.addEventListener('input', (event) => {
+    const phoneNumber = event.target.value;
+    const phoneNumberPatternValue = /[1-1010-1]/;
+
+    if (!phoneNumberPatternValue.test(phoneNumber)) {
+        phoneNumberInput.setCustomValidity("Voer a.u.b een geldig telefoonnummer in.");
+    } else {
+        phoneNumberInput.setCustomValidity("");
+    }
+});
