71a39bc18f42f01a4f29bb3de40c6b0588fe492e
thijew <thijewind@hotmail.com>
2023-06-29 23:16:47 +0200

    FIX Form validation

--- static/scripts/form-validation.js
+++ static/scripts/form-validation.js
@@ -1,26 +1,66 @@
 const nameInput = document.querySelector('input#name');
+const phoneNumberInput = document.querySelector('input#phone');
+const chickensInput = document.querySelector('input#chickens');
+const dateInput = document.querySelector('input#date-input');
+const inputTime = document.querySelector('select#time-input');
 
-nameInput.addEventListener('input', (event) => {
+// Check name
+nameInput.addEventListener('keyup', (event) => {
     const name = event.target.value;
-    const alphabeticRegex = /[A-Z/a-z]/;
+    const alphabeticRegex = /^[A-Za-z\s]+$/;
 
     if (!alphabeticRegex.test(name)) {
-        nameInput.setCustomValidity("Voer a.u.b alleen letters in.");
+        nameInput.setCustomValidity("Please enter letters only.");
     } else {
         nameInput.setCustomValidity("");
     }
 });
 
-
-const phoneNumberInput = document.querySelector('input#phone');
-
-phoneNumberInput.addEventListener('input', (event) => {
+// Check phone number
+phoneNumberInput.addEventListener('keyup', (event) => {
     const phoneNumber = event.target.value;
-    const phoneNumberPatternValue = /[1-10]/;
+    const phoneNumberPattern = /^\+?[0-9]{6,14}$/;
 
-    if (!phoneNumberPatternValue.test(phoneNumber)) {
-        phoneNumberInput.setCustomValidity("Voer a.u.b een geldig telefoonnummer in.");
+    if (!phoneNumberPattern.test(phoneNumber)) {
+        phoneNumberInput.setCustomValidity("Please enter a valid phone number.");
     } else {
         phoneNumberInput.setCustomValidity("");
+    }
+});
+
+// Check number of chickens
+chickensInput.addEventListener('change', (event) => {
+    const chickens = event.target.value;
+    const numberRegex = /^[0-9]+$/;
+
+    if (!numberRegex.test(chickens)) {
+        chickensInput.setCustomValidity("Please enter a valid number.");
+    } else if (chickens < 0 || chickens > 20) {
+        chickensInput.setCustomValidity("Please enter a number between 0 and 20.");
+    } else {
+        chickensInput.setCustomValidity("");
+    }
+});
+
+// Check reservation date
+dateInput.addEventListener('change', (event) => {
+    const selectedDate = event.target.value;
+    const today = new Date().toISOString().split('T')[0];
+
+    if (selectedDate < today) {
+        dateInput.setCustomValidity("Please select a future date.");
+    } else {
+        dateInput.setCustomValidity("");
+    }
+});
+
+// Check reservation time
+inputTime.addEventListener('change', (event) => {
+    const selectedTime = event.target.value;
+
+    if (selectedTime === "") {
+        inputTime.setCustomValidity("Please select a reservation time.");
+    } else {
+        inputTime.setCustomValidity("");
     }
 });
--- static/scripts/weather-api.js
+++ static/scripts/weather-api.js
@@ -1,16 +1,17 @@
-const weatherApi = document.querySelector('.weatherApi');
+// const weatherApi = document.querySelector('.weatherApi');
 
 
-async function fetchWeather() {
-    try {
-        const url = "https://api.open-meteo.com/v1/forecast?latitude=52.56&longitude=4.61&daily=temperature_2m_max,temperature_2m_min,rain_sum&current_weather=true&timezone=Europe%2FBerlin"
-        let response = await fetch(url);
-        let weather = await response.json();
-        console.log(weather);
+// async function fetchWeather() {
+//     try {
+//         const url = "https://api.open-meteo.com/v1/forecast?latitude=52.56&longitude=4.61&daily=temperature_2m_max,temperature_2m_min,rain_sum&current_weather=true&timezone=Europe%2FBerlin"
+//         let response = await fetch(url);
+//         let weather = await response.json();
+//         console.log(weather);
 
-    } catch (error) {
-        console.log('Error fetching weather data:', error);
-    }
-}
-fetchWeather();
-console.log(weatherApi);
+//     } catch (error) {
+//         console.log('Error fetching weather data:', error);
+//     }
+// }
+// fetchWeather();
+// console.log(weatherApi)
+
